44ab28d1edd832d980d7e4e693cdc5f2e0696b4a
simonepozzobon <info@simonepozzobon.com>
2018-05-08 18:25:43 +0200

    Aggiunge il template per la ricerca libera e quello per il loop dei film

--- wp-content/themes/cineteca-inail/includes/loop/loop-film.php
+++ wp-content/themes/cineteca-inail/includes/loop/loop-film.php
@@ -4,23 +4,27 @@ function cineteca_inail_get_the_film_loop_from_query($loop,$add)
 {
 ?>
 	<section class="row" role="content-info">
-		<div class="col-xs-12 col-sm-9 col-md-9 col-lg-9 bb-query-container">
-<?php	
+		<div class="col-12 bb-query-container">
+<?php
 	$uid = cineteca_inail_generate_uid();
 	if ( $loop->have_posts() ) :
 		while ( $loop->have_posts() ) : $loop->the_post(); ?>
 			<?php $postinfo = cineteca_inail_get_film_info_from_id(get_the_ID()); ?>
-			<div class="row bb-query-result-row" id="bb-q-result-<?php echo get_the_ID();?>">
-				<div class="col-sm-1 col-md-1 col-lg-1 bb-result-image">
+			<div class="row bb-query-result-row pb-5" id="bb-q-result-<?php echo get_the_ID();?>">
+				<div class="col-md-4">
 					<div class="bb-result-image-thumb">
-					<?php if( has_post_thumbnail() ): ?>
-						<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(array(132,98)); ?></a>
-					<?php else: ?>
-						<a href="#"><img alt="default thumbnail" src="<?php echo get_template_directory_uri().'/images/greythumb.png'; ?>"/></a>
-					<?php endif; ?>
+						<?php if( has_post_thumbnail() ): ?>
+							<a href="<?php the_permalink(); ?>">
+								<img class="img-fluid" src="<?php the_post_thumbnail_url(); ?>" />
+							</a>
+						<?php else: ?>
+							<a href="#">
+								<img class="img-fluid" alt="default thumbnail" src="<?php echo get_template_directory_uri().'/images/greythumb.png'; ?>"/>
+							</a>
+						<?php endif; ?>
 					</div>
 				</div>
-				<div class="col-sm-9 col-md-9 col-lg-9 bb-result-content-info">
+				<div class="col-md-8">
 					<div class="bb-result-title">
 						<h2>
 							<a href="<?php the_permalink(); ?>" title="<?php echo get_the_title(); ?>">
@@ -29,27 +33,32 @@ function cineteca_inail_get_the_film_loop_from_query($loop,$add)
 						</h2>
 					</div>
 					<div class="bb-result-info">
-						<?php echo $postinfo['genere'][0]; ?> - 
-						<?php echo $postinfo['anno_di_produzione'][0]; ?> - 
-						<?php echo $postinfo['nazionalità'][0]; ?> - 
+						<?php echo $postinfo['genere'][0]; ?> -
+						<?php echo $postinfo['anno_di_produzione'][0]; ?> -
+						<?php echo $postinfo['nazionalità'][0]; ?> -
 						<?php echo $postinfo['durata'][0]; ?>' -
 						Et&agrave; consigliata: <?php echo $postinfo['età_consigliata'][0]; ?>
 					</div>
-					
 					<div class="bb-result-dirandcast">
 						Un film di <span class="bb-heavier"><?php echo $postinfo['regia'][0];?></span><br>
-						Con <span class="bb-heavier"><?php echo $postinfo['cast_and_credits'][0]; ?></span>.
+						Con <span class="bb-heavier"><?php echo $postinfo['cast_and_credits'][0]; ?></span>
 					</div>
-					<?php if($add): ?>
-					<span class="btn bb-add-btn"><a class="btn" onclick="cineteca_inail_addFilmToRassegna(<?php echo get_the_ID();?>,<?php echo $uid;?>)">Aggiungi alla rassegna</a></span>
-					<?php else: ?>
-					<span class="btn bb-remove-btn"><a class="btn" onclick="cineteca_inail_removeFilmFromRassegna(<?php echo get_the_ID();?>,<?php echo $uid;?>)">Rimuovi</a></span>
-					<?php endif; ?>
+					<div class="buttons pt-3">
+						<?php if($add): ?>
+							<div class="bb-add-btn">
+								<a class="btn btn-light" onclick="cineteca_inail_addFilmToRassegna(<?php echo get_the_ID();?>,<?php echo $uid;?>)">Aggiungi alla rassegna</a>
+							</div>
+						<?php else: ?>
+							<div class="bb-remove-btn">
+								<a class="btn btn-danger text-white" onclick="cineteca_inail_removeFilmFromRassegna(<?php echo get_the_ID();?>,<?php echo $uid;?>)">Rimuovi</a>
+							</div>
+						<?php endif; ?>
+					</div>
 				</div>
 			</div>
 		<?php endwhile; ?>
 		</div>
-		
+
 		<?php if (  $loop->max_num_pages > 1 ) : ?>
 			<div id="nav-below" class="navigation">
 				<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Previous', 'domain' ) ); ?></div>
@@ -57,18 +66,18 @@ function cineteca_inail_get_the_film_loop_from_query($loop,$add)
 			</div>
 		<?php endif;
 	else:
-?>   
+?>
 	<div class="bb-query-result-row">
 		<h2 class="bb-query-no-result">
 			<?php echo _e('Nessun risultato trovato','cineteca-inail'); ?>
 		</h2>
 	</div>
-<?php 
+<?php
 	endif;
 ?>
 	</section>
 
-<?php 
+<?php
 	wp_reset_postdata();
 }
 
@@ -87,7 +96,7 @@ function cineteca_inail_get_the_5_film_rand_loop_from_query($loop,$add){
 		$random_numbers = array();
 
 		if( $loop->post_count > 5 )
-		{    
+		{
 			while(sizeof($random_numbers) < 5)
 			{
 				$r = rand(0,$loop->post_count-1);
@@ -96,7 +105,7 @@ function cineteca_inail_get_the_5_film_rand_loop_from_query($loop,$add){
 			}
 		}
 
-		while ( $loop->have_posts() ) : $loop->the_post(); 
+		while ( $loop->have_posts() ) : $loop->the_post();
 
 			if( $loop->post_count > 5 )
 			{
@@ -125,13 +134,13 @@ function cineteca_inail_get_the_5_film_rand_loop_from_query($loop,$add){
 						</h2>
 					</div>
 					<div class="bb-result-info">
-						<?php echo $postinfo['genere'][0]; ?> - 
-						<?php echo $postinfo['anno_di_produzione'][0]; ?> - 
-						<?php echo $postinfo['nazionalità'][0]; ?> - 
+						<?php echo $postinfo['genere'][0]; ?> -
+						<?php echo $postinfo['anno_di_produzione'][0]; ?> -
+						<?php echo $postinfo['nazionalità'][0]; ?> -
 						<?php echo $postinfo['durata'][0]; ?>' -
 						Et&agrave; consigliata: <?php echo $postinfo['età_consigliata'][0]; ?>
 					</div>
-					
+
 					<div class="bb-result-dirandcast">
 						Un film di <span class="bb-heavier"><?php echo $postinfo['regia'][0];?></span><br>
 						Con <span class="bb-heavier"><?php echo $postinfo['cast_and_credits'][0]; ?></span>.
@@ -145,7 +154,7 @@ function cineteca_inail_get_the_5_film_rand_loop_from_query($loop,$add){
 			</div>
 		<?php endwhile; ?>
 		</div>
-		
+
 		<?php if (  $loop->max_num_pages > 1 ) : ?>
 			<div id="nav-below" class="navigation">
 				<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Previous', 'domain' ) ); ?></div>
@@ -153,18 +162,18 @@ function cineteca_inail_get_the_5_film_rand_loop_from_query($loop,$add){
 			</div>
 		<?php endif;
 	else:
-?>   
+?>
 	<div class="bb-query-result-row">
 		<h2 class="bb-query-no-result">
 			<?php echo _e('Nessun risultato trovato','cineteca-inail'); ?>
 		</h2>
 	</div>
-<?php 
+<?php
 	endif;
 ?>
 	</section>
 
-<?php 
+<?php
 	wp_reset_postdata();
 }
 
@@ -188,13 +197,13 @@ function cineteca_inail_get_the_film_title_loop_from_query($loop)
 	<?php endwhile; ?>
 		</div>
 <?php else:
-?>   
+?>
 	<div class="bb-query-result-row">
 		<h2 class="bb-query-no-result">
 			<?php echo _e('Nessun risultato trovato','cineteca-inail'); ?>
 		</h2>
 	</div>
-<?php 
+<?php
 	endif;
 ?>
 	</section>
@@ -214,7 +223,7 @@ function cineteca_inail_get_film_loop_for_mail($loop)
 	<?php endwhile; ?>
 <?php endif; ?>
 	</ul>
-<?php 
+<?php
 	wp_reset_postdata();
 }
 
--- wp-content/themes/cineteca-inail/templates/template-ricerca-libera.php
+++ wp-content/themes/cineteca-inail/templates/template-ricerca-libera.php
@@ -3,7 +3,7 @@
   Template Name: Ricerca Libera
   Description: Template ricerca libera
 */
-	get_header(); 
+	get_header();
 ?>
 <?php
 
@@ -13,29 +13,29 @@
 		var_dump($formitems);
 
 	if (isset($_POST['all-films'])){
-		
+
 		$filmresults = cineteca_inail_query_all_films();
 
-	} else if( 
+	} else if(
 		isset($_POST['titolo']) 		|| isset($_POST['regia']) 		 ||
 		isset($_POST['cast']) 			|| isset($_POST['nazionalita'])  ||
 		isset($_POST['tematiche']) 	|| isset($_POST['tipologia']) 	 ||
 		isset($_POST['genere']) 		|| isset($_POST['eta-studenti']) ||
 		isset($_POST['anno_produzione'])
 	){
-	
+
 		if (DEBUG){
 			echo "here";
 		}
 
-		if(	
+		if(
 			$_POST['titolo'] != "" 						|| $_POST['anno_produzione'] != "Decennio"			||
-			$_POST['regia'] != ""  	  					|| $_POST['cast'] != "" 							|| 
+			$_POST['regia'] != ""  	  					|| $_POST['cast'] != "" 							||
 			$_POST['nazionalita'] != ""  				|| $_POST['tipologia'] != "Seleziona la tipologia"  ||
 			$_POST['genere'] != "Seleziona il genere"	|| $_POST['eta-studenti'] != "Età consigliata"		||
 			isset($_POST['tematiche'])
 		){
-			
+
 			if( DEBUG ){
 				echo "there";
 				var_dump( $_POST );
@@ -74,8 +74,8 @@
 			}
 
 			if (isset($_POST['do-query'])){
-				$filmresults  = cineteca_inail_query_builder( 
-								$_POST['titolo'], 
+				$filmresults  = cineteca_inail_query_builder(
+								$_POST['titolo'],
 								$_POST['tipologia'],
 								$_POST['genere'],
 								$_POST['nazionalita'],
@@ -83,9 +83,9 @@
 								$_POST['cast'],
 								$_POST['tematiche'],
 								$_POST['eta-studenti'],
-								$_POST['anno_produzione'] 
+								$_POST['anno_produzione']
 							);
-			} 
+			}
 
 			$default_title	 = $_POST['titolo'];
 			$default_regia	 = $_POST['regia'];
@@ -109,137 +109,156 @@
 	if( isset($filmresults) )
 		wp_enqueue_style('cineteca.query.results');
 ?>
-<main class="container bb-main-wrapper">
-	<section class="row" role="content-info">
-		<div class="col-xs-12 col-sm-9 col-md-9 col-lg-9 bb-search bb-search-free" id="bb-search-free-container">
-			<div class="bb-search-free-title bb-search-title">
-				<span><img src="<?php echo get_template_directory_uri().'/images/magnifier.png'; ?>" alt="lente di ingrandimento"/></span>
-				<h1><?php echo _e('Ricerca libera','cineteca-inail'); ?></h1>
-			</div>
 
-			<div class="bb-search-form-container bb-search-free-form-container">
-				<form method="POST" class="form">
+<main role="main" class="container pb-5">
+	<div class="row">
+		<div class="col-md-8">
+			<!-- Parte Centrale -->
+			<div class="row">
+				<div class="col">
+					<div class="row bb-home-sect-title">
+						<h1><?php echo _e('Ricerca Libera','cineteca-inail'); ?></h1>
+					</div>
+					<div class="row">
+						<div id="content" class="col content">
 
-					<div class="form-group form-inline bb-two-group">
-						<div class="col-sm-6 col-md-6 col-lg-6">
-							<label class="bb-free-search-lbl" for="titolo">
-								<?php echo _e('Titolo','cineteca-inail'); ?>
+							<form class="form" method="post">
 
-							</label>
-							<br>
-							<input  type="text" 
-									class="form-control" 
-									name="titolo"
-								<?php if( isset($default_title) ): ?>
-									value="<?php echo $default_title;?>"
-								<?php endif; ?>
-							/>
+								<div class="row">
+									<div class="form-group col-md-6">
+										<label class="bb-free-search-lbl" for="titolo"><?php echo _e('Titolo','cineteca-inail'); ?></label>
+										<input  type="text" class="form-control" name="titolo"
+											<?php if( isset($default_title) ): ?>
+												value="<?php echo $default_title;?>"
+											<?php endif; ?>
+										/>
+									</div>
+									<div class="form-group col-md-6">
+										<label class="bb-free-search-lbl" for="regia"><?php echo _e('Regia','cineteca-inail'); ?></label>
+										<input  type="text" class="form-control" name="regia"
+											<?php if( isset($default_regia) ): ?>
+												value="<?php echo $default_regia;?>"
+											<?php endif; ?>
+										/>
+									</div>
+								</div>
+
+								<div class="row">
+									<div class="form-group col-md-6">
+										<label class="bb-free-search-lbl" for="cast"><?php echo _e('Cast','cineteca-inail'); ?></label>
+										<input  type="text" class="form-control" name="cast"
+											<?php if( isset($default_cast) ): ?>
+												value="<?php echo $default_cast;?>"
+											<?php endif; ?>
+										/>
+									</div>
+									<div class="form-group col-md-6">
+										<label class="bb-free-search-lbl" for="nazionalita"><?php echo _e('Nazionalit&agrave;','cineteca-inail'); ?></label>
+										<input  type="text" class="form-control" name="nazionalita"
+											<?php if( isset($default_country) ): ?>
+												value="<?php echo $default_country;?>"
+											<?php endif; ?>
+										/>
+									</div>
+								</div>
+
+								<div class="row">
+									<div class="form-group col-md-6">
+										<label class="bb-free-search-lbl" for="anno_produzione"><?php echo _e('Anno di Produzione','cineteca-inail'); ?></label>
+										<?php 	$decade = $formitems['anno_produzione'];	?>
+										<select class="form-control" name="anno_produzione">
+											<option><?php echo _e('Decennio','cineteca-inail'); ?></option>
+											<?php foreach( $decade as $decadeitem ) { ?>
+												<option	<?php if( isset($default_decade) && $default_decade == $decadeitem ) echo "selected";?> ><?php echo $decadeitem; ?></option>
+											<?php } ?>
+										</select>
+									</div>
+									<div class="form-group col-md-6">
+										<label class="bb-free-search-lbl" for="tipologia"><?php echo _e('Tipologia','cineteca-inail'); ?></label>
+										<?php $tipology = $formitems['tipologia']; ?>
+										<select class="form-control" name="tipologia">
+											<option><?php echo _e('Seleziona la tipologia','cineteca-inail'); ?></option>
+											<?php foreach( $tipology as $tipologyitem ) { ?>
+												<option <?php if( isset($default_type) && $default_type == $tipologyitem ) echo "selected";	?> ><?php echo $tipologyitem; ?></option>
+											<?php } ?>
+										</select>
+									</div>
+								</div>
+
+								<div class="row">
+									<div class="form-group col-md-6">
+										<label class="bb-free-search-lbl" for="genere"><?php echo _e('Genere','cineteca-inail'); ?></label>
+										<?php 	$genere = $formitems['genere'];	?>
+										<select class="form-control" name="genere">
+											<option><?php echo _e('Seleziona il genere','cineteca-inail'); ?></option>
+											<?php foreach( $genere as $genereitem ) { ?>
+												<option <?php if( isset($default_genre) && $default_genre == $genereitem ) echo "selected";	?> ><?php echo $genereitem; ?></option>
+											<?php } ?>
+										</select>
+									</div>
+									<div class="form-group col-md-6">
+										<label class="bb-free-search-lbl" for="eta-studenti"><?php echo _e('Et&agrave; Studenti','cineteca-inail'); ?></label>
+										<?php 	$age = $formitems['anni_studenti']; ?>
+										<select class="form-control" name="eta-studenti">
+											<option><?php echo _e('Et&agrave; consigliata','cineteca-inail'); ?></option>
+											<?php foreach( $age as $ageitem ) { ?>
+												<option <?php if( isset($default_age) && $default_age == $ageitem ) echo "selected"; ?> ><?php echo $ageitem; ?></option>
+											<?php } ?>
+										</select>
+									</div>
+								</div>
+
+								<div class="row">
+									<div class="form-group col-md-12">
+										<h2 class="bb-search-subtitle">Quali tematiche ti interessano?</h2>
+										<?php $keywords = $formitems["tematiche"]; ?>
+										<div class="bb-keywords">
+											<?php $i = 0;
+												foreach( $keywords as $keyword ) { ?>
+													<div class="bb-keyword">
+														<input type="checkbox" id="inlineCheckbox<?php echo $i; ?>" name="tematiche[]" value="<?php echo $keyword; ?>">
+														<label for="inlineCheckbox<?php echo $i; ?>" class="checkbox-inline bb-sublbl">
+															<?php echo $keyword; ?>
+														</label>
+													</div>
+											<?php $i++;
+												}
+											?>
+										</div>
+									</div>
+								</div>
+
+								<input class="btn btn-dark bb-free-src-btn" name="do-query" type="submit" value="Cerca"/>
+								<input class="btn btn-dark bb-free-src-all-film-btn" name="all-films" type="submit" value="Tutti i Film"/>
+
+							</form>
+
+							<div class="results pt-5">
+								<?php
+									if( isset($filmresults) )
+										cineteca_inail_get_the_film_loop_from_query( $filmresults, true );
+
+									if( DEBUG && isset($filmresults) )
+									{
+										echo '<br><br><hr><br><br>'; /// -_-
+										var_dump($filmresults);
+										echo '<hr>';
+									}
+								?>
+							</div>
 						</div>
-						<div class="col-sm-6 col-md-6 col-lg-6">
-							<label class="bb-free-search-lbl" for="regia"><?php echo _e('Regia','cineteca-inail'); ?></label>
-							<br>
-							<input  type="text" 
-									class="form-control" 
-									name="regia"
-								<?php if( isset($default_regia) ): ?>
-									value="<?php echo $default_regia;?>"
-								<?php endif; ?>
-							/>
-						</div>
 					</div>
-					<div class="form-group form-inline bb-two-group">
-						<div class="col-sm-6 col-md-6 col-lg-6 bb-free-search-second-row">
-							<label class="bb-free-search-lbl" for="cast"><?php echo _e('Cast','cineteca-inail'); ?></label>
-							<br>
-							<input  type="text" 
-									class="form-control" 
-									name="cast"
-								<?php if( isset($default_cast) ): ?>
-									value="<?php echo $default_cast;?>"
-								<?php endif; ?>
-							/>
-						</div>
-						<div class="col-sm-6 col-md-6 col-lg-6 bb-free-search-second-row">	
-							<label class="bb-free-search-lbl" for="nazionalita"><?php echo _e('Nazionalit&agrave;','cineteca-inail'); ?></label>
-							<br>
-							<input  type="text" 
-									class="form-control" 
-									name="nazionalita"
-								<?php if( isset($default_country) ): ?>
-									value="<?php echo $default_country;?>"
-								<?php endif; ?>
-							/>
-						</div>
-					</div>
-					<div class="form-group form-inline bb-two-group bb-free-search-third-row">
-						<?php 	$decade = $formitems['anno_produzione'];	?>
-						<select class="form-control" name="anno_produzione">
-							<option><?php echo _e('Decennio','cineteca-inail'); ?></option>
-						<?php foreach( $decade as $decadeitem ) { ?>
-							<option	<?php if( isset($default_decade) && $default_decade == $decadeitem ) echo "selected";?> ><?php echo $decadeitem; ?></option>
-						<?php } ?>
-						</select>
-
-						<?php 	$tipology = $formitems['tipologia'];	?>
-						<select class="form-control" name="tipologia">
-							<option><?php echo _e('Seleziona la tipologia','cineteca-inail'); ?></option>
-						<?php foreach( $tipology as $tipologyitem ) { ?>
-							<option <?php if( isset($default_type) && $default_type == $tipologyitem ) echo "selected";	?> ><?php echo $tipologyitem; ?></option>
-						<?php } ?>
-						</select>		
-					</div>
-					<div class="form-group form-inline bb-two-group bb-free-search-fourth-row">
-						<?php 	$genere = $formitems['genere'];	?>
-						<select class="form-control" name="genere">
-							<option><?php echo _e('Seleziona il genere','cineteca-inail'); ?></option>
-						<?php foreach( $genere as $genereitem ) { ?>
-							<option <?php if( isset($default_genre) && $default_genre == $genereitem ) echo "selected";	?> ><?php echo $genereitem; ?></option>
-						<?php } ?>
-						</select>
-						<?php 	$age = $formitems['anni_studenti']; ?>
-						<select class="form-control" name="eta-studenti">
-							<option><?php echo _e('Et&agrave; consigliata','cineteca-inail'); ?></option>
-						<?php foreach( $age as $ageitem ) { ?>
-							<option <?php if( isset($default_age) && $default_age == $ageitem ) echo "selected"; ?> ><?php echo $ageitem; ?></option>
-						<?php } ?>
-						</select>
-					</div>
-					
-					<?php $keywords = $formitems["tematiche"]; ?>
-					<div class="form-group bb-free-search-keywords">
-						<h2 class="bb-search-subtitle">Quali tematiche ti interessano?</h2>
-						<br>
-						<?php $i = 0;
-							foreach( $keywords as $keyword ){ ?>
-								<label for="inlineCheckbox<?php echo $i; ?>" class="checkbox-inline bb-sublbl">
-									<?php echo $keyword; ?>
-								</label>
-								<input type="checkbox" id="inlineCheckbox<?php echo $i; ?>" name="tematiche[]" value="<?php echo $keyword; ?>">
-								<span class="bb-white-space"></span>
-						<?php $i++;
-							}
-						?>
-					</div>
-
-					<input class="btn bb-free-src-btn" name="do-query" type="submit" value="Cerca"/>			
-					<input class="btn bb-free-src-all-film-btn" name="all-films" type="submit" value="Tutti i Film"/>
-				</form>
+				</div>
 			</div>
 		</div>
-		<?php get_sidebar(); ?>
-	</section>
-
-	<?php 
-		if( isset($filmresults) )
-			cineteca_inail_get_the_film_loop_from_query($filmresults,true);
-
-		if( DEBUG && isset($filmresults) )
-		{
-			echo '<br><br><hr><br><br>'; /// -_-
-			var_dump($filmresults);
-			echo '<hr>';
-		}
-	?>
-
+		<div class="col-md-4">
+			<!-- Sidebar -->
+			<div class="row">
+				<div class="col-12">
+					<?php get_sidebar(); ?>
+				</div>
+			</div>
+		</div>
+	</div>
 </main>
 <?php get_footer(); ?>
